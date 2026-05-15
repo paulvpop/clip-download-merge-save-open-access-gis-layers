098b1694927f38a4ffd40b26ab8779ead2c766f5
Paul Pop <159306776+paulvpop@users.noreply.github.com>
2026-05-15 13:30:46 +0530

    Update R_script_to_merge_clip_and_save_google_open_buildings_data.R

--- R_script_to_merge_clip_and_save_google_open_buildings_data.R
+++ R_script_to_merge_clip_and_save_google_open_buildings_data.R
@@ -1,4 +1,8 @@
-# The following is an R script to load, clip to study area, and save Google Open Buildings data
+Author: Paul Pop
+Affiliation: BIRD Lab, ATREE, Bengaluru (PI: Rajkamal Goswami)
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+# This is an R script to load, clip to study area, and save Google Open Buildings data in Parquet format.
 
 # First set your working directory
 # Ctrl+Shift+H
@@ -30,15 +34,14 @@ clip_boundary <- st_read("D:/GIS_files/Arunachal/arunachal_shapefile/Arunachal_P
 # Simple feature collection with 1 feature and 12 fields
 # Geometry type: POLYGON
 # Dimension:     XYZ
-
 # Bounding box:  xmin: 91.54646 ymin: 26.65084 xmax: 97.4115 ymax: 29.46173
 # z_range:       zmin: 0 zmax: 0
 # Geodetic CRS:  WGS 84
 
-# Install some more necessary packages and load them (uncomment below to do it)
-# install.packages(c("arrow","duckdb"))
-library(arrow)
+# Install one more necessary package and load them (uncomment below to do it)
+# install.packages("duckdb")
 library(duckdb)
+# DuckDB is an embedded SQL database optimized for analytical queries.
 
 # Load shapefile into DuckDB
 clip_boundary_wkt <- st_as_text(st_union(clip_boundary))
@@ -46,18 +49,22 @@ clip_boundary_wkt <- st_as_text(st_union(clip_boundary))
 # Prepare all the files for processing
 all_files <- paste(shQuote(file_paths), collapse = ", ")
 
-# Read CSV with arrow and convert WKT to geometry in Parquet
- con <- dbConnect(duckdb())
+# Create a connection to an in-memory DuckDB database:
+ con <- dbConnect(duckdb()) # No file is created on disk (in-memory means temporary)
+# Install and loads DuckDB's spatial extension
  dbExecute(con, "INSTALL spatial; LOAD spatial;")
+# This enables geographic/spatial functions like ST_GeomFromText()
+# The extension adds GIS capabilities to DuckDB.
 
 # The following dbExecute()command executes a SQL query within DuckDB that reads multiple CSV files,
 # filters buildings within a boundary, and saves the results as a Parquet file.
+# Note that in SELECT step, the other fields area_in_meters, confidence, and full_plus_code have not 
+# been selected to save computationaltime. You can retain them by adding them to right after longitude, 
+# seperated by commas.
 dbExecute(con, sprintf("
   COPY (
     SELECT ST_GeomFromText(geometry) AS geom,
-           latitude, longitude             # Note that in this step, the other fields area_in_meters, confidence,
-     # and full_plus_code have not been selected to save computational
-     # time. You can retain them by adding them to right after longitude, seperated by commas
+           latitude, longitude             
     FROM read_csv_auto([%s])
     WHERE ST_Within(ST_GeomFromText(geometry), ST_GeomFromText('%s'))
   ) TO 'all_buildings_clipped.parquet' WITH (FORMAT PARQUET)
